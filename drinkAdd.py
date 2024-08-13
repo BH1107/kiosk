be6c625c609ee0c639131d72afd5d6db466403a7
BH <hiantonio1107@gmail.com>
2024-08-13 14:23:54 +0900

    last

--- drinkAdd.py
+++ drinkAdd.py
@@ -1,10 +1,12 @@
 def add_menu(item_list): 
-        print(f'1. {item_list.item[0]}')
-        print(f'2. {item_list.item[1]}')
-        print(f'3. {item_list.item[2]}')
-        print(f'4. {item_list.item[3]}')
-        print(f'5. {item_list.item[4]}')
-        print(f'6. {item_list.item[5]}')
+        item_key = item_list.item.keys()
+        item_key_list = list(item_key)
+        print('1. 아메리카노')
+        print(f'2. 카페라떼')
+        print(f'3. 콜드브루')
+        print(f'4. 에스프레소')
+        print(f'5. 아이스티')
+        print(f'6. 말차라떼')
 
         print('10. 동작취소')
 
@@ -12,25 +14,25 @@ def add_menu(item_list):
             user_input = input()
 
             print(f'선택 : {user_input}')
-            if user_input == 1:
-                item_list.current_item.append(item_list.item[0])
+            if user_input == '1':
+                item_list.current_item.append(item_key_list[0])
                 break
-            elif user_input == 2:
-                item_list.current_item.append(item_list.item[1])
+            elif user_input == '2':
+                item_list.current_item.append(item_key_list[1])
                 break
-            elif user_input == 3:
-                item_list.current_item.append(item_list.item[2])
+            elif user_input == '3':
+                item_list.current_item.append(item_key_list[2])
                 break
-            elif user_input == 4:
-                item_list.current_item.append(item_list.item[3])
+            elif user_input == '4':
+                item_list.current_item.append(item_key_list[3])
                 break
-            elif user_input == 5:
-                item_list.current_item.append(item_list.item[4])
+            elif user_input == '5':
+                item_list.current_item.append(item_key_list[4])
                 break
-            elif user_input == 6:
-                item_list.current_item.append(item_list.item[5])
+            elif user_input == '6':
+                item_list.current_item.append(item_key_list[5])
                 break
-            elif user_input == 10:
+            elif user_input == '10':
                 print('취소')
                 break
             else: 
