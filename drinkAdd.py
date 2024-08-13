8168a55c97e9b6a16f870f8b4c5a8b2036218da3
BH <hiantonio1107@gmail.com>
2024-08-13 12:38:27 +0900

    fix append and ItemList

--- drinkAdd.py
+++ drinkAdd.py
@@ -1,11 +1,10 @@
 def add_menu(item_list): 
-        selected = []
-        print(f'1. {item_list[0]}')
-        print(f'2. {item_list[1]}')
-        print(f'3. {item_list[2]}')
-        print(f'4. {item_list[3]}')
-        print(f'5. {item_list[4]}')
-        print(f'6. {item_list[5]}')
+        print(f'1. {item_list.item[0]}')
+        print(f'2. {item_list.item[1]}')
+        print(f'3. {item_list.item[2]}')
+        print(f'4. {item_list.item[3]}')
+        print(f'5. {item_list.item[4]}')
+        print(f'6. {item_list.item[5]}')
 
         print('10. 동작취소')
 
@@ -14,22 +13,22 @@ def add_menu(item_list):
 
             print(f'선택 : {user_input}')
             if user_input == 1:
-                selected = item_list[0]
+                item_list.current_item.append(item_list.item[0])
                 break
             elif user_input == 2:
-                selected = item_list[1]
+                item_list.current_item.append(item_list.item[1])
                 break
             elif user_input == 3:
-                selected = item_list[2]
+                item_list.current_item.append(item_list.item[2])
                 break
             elif user_input == 4:
-                selected = item_list[3]
+                item_list.current_item.append(item_list.item[3])
                 break
             elif user_input == 5:
-                selected = item_list[4]
+                item_list.current_item.append(item_list.item[4])
                 break
             elif user_input == 6:
-                selected = item_list[5]
+                item_list.current_item.append(item_list.item[5])
                 break
             elif user_input == 10:
                 print('취소')
